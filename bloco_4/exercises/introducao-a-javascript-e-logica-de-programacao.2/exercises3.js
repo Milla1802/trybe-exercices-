aeacb07782a9b673b0f2f5ac99b8076d333b11f4
Camila Arruda <camilaam1802@gmail.com>
2021-02-25 18:53:25 -0300

    Resolvendo a questão 3

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/exercises3.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/exercises3.js
@@ -1,8 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let media = 0;
 let soma = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-  soma += numbers[i];
+    soma += numbers[i];
+    media = soma / numbers.length;
+    
 }
-
-console.log(soma);
+  
+console.log(media);
