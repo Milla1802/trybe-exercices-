f6b840afd5b290ed63e77d6ea27a148d64f2d879
Camila Arruda <camilaam1802@gmail.com>
2021-02-27 13:50:53 -0300

    último commit dessa atividade com a questão 9.

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/exercise9.js
@@ -0,0 +1,7 @@
+let arrayNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]; 
+
+for (let index = 0; index < arrayNum.length; index++) {
+    
+    let divisao = arrayNum[index] / 2; 
+    console.log(divisao); 
+}
