40f826553c7c32354dae7870a8781c176909d99f
Camila Arruda <camilaam1802@gmail.com>
2021-03-01 18:46:12 -0300

    Exercício 5 feito

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/dia4.4/exercise.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/dia4.4/exercise.js
@@ -2,8 +2,8 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+ 
 };
-
 
 
 // Exercício 1: 
@@ -24,4 +24,21 @@ for (let key in info) {
 //Exerćicio 4: 
 for (let key in info) {
     console.log(info[key]);
+}
+
+// Exercício 5:
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente : 'Sim'
+};
+
+for (let key in info){
+    if (info[key] === info.recorrente && info[key] === "Sim" && info2[key] === "Sim"){
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[key] + " e " + info2[key]); 
+    }
 }
