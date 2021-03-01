815e2270dc9d5cb2ce64355d5de7f2fc524eb7d1
Camila Arruda <camilaam1802@gmail.com>
2021-03-01 19:35:08 -0300

    Questão 4 da parte 2 feita

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/dia4.4/exercises2.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/dia4.4/exercises2.js
@@ -30,3 +30,33 @@ function maiorNum(array) {
 }
 
 console.log(maiorNum([2, 4, 6, 7, -10, 0, 3]));
+
+// Exercício 4: 
+
+function maiorNome(nomes) {
+    
+    let maisCaracteres = nomes[0]; 
+
+    for (let key in nomes){
+        
+        if (maisCaracteres.length < nome[key].length){
+            maisCaracteres = nomes[key]; 
+        }
+
+        return maisCaracteres; 
+    }
+
+    console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+}
+
+function maiorPalavra(palavras) {
+    let maiorPalavra = palavras[0];
+    for (let indice in palavras) {
+      if (maiorPalavra.length < palavras[indice].length) {
+        maiorPalavra = palavras[indice];
+      }
+    }
+    return maiorPalavra;
+  }
+  
+  console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); 
