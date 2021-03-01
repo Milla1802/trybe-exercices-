1eec19bdd5428107ed4ea468e2d7302a807dc6b5
Camila Arruda <camilaam1802@gmail.com>
2021-03-01 18:32:07 -0300

    Exercício 4 feito

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/dia4.4/exercise.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.2/dia4.4/exercise.js
@@ -2,7 +2,9 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  };
+};
+
+
 
 // Exercício 1: 
 
@@ -17,4 +19,9 @@ console.log(info);
 
 for (let key in info) {
     console.log(key);
+}
+
+//Exerćicio 4: 
+for (let key in info) {
+    console.log(info[key]);
 }
