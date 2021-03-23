b2b7175d3a1f27de883d5c5dbf76bdbd15b99c59
Camila Arruda <camilaam1802@gmail.com>
2021-03-23 18:37:44 -0300

    questão 2, part 1

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -11,3 +11,12 @@ const testingScope = escopo => {
 }
 
       testingScope(false);
+
+      
+// Questão 2 - part1 
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a,b) => a - b );
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente`);
+
+
